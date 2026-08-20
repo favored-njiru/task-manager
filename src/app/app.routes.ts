4a3be55e457d46a23a61fc3556fb0da7c3bb2801
Kelly Ndegwa <kelly.ndegwa@savannahinformatics.com>
2026-08-20 11:28:17 +0300

    feat: add tasklist features

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,13 @@
 import { Routes } from '@angular/router';
 import { Login } from './components/login/login';
-import { Dashboard } from './components/dashboard/dashboard';
+
 import { TaskList } from './components/task-list/task-list';
 export const routes: Routes = [
     {
         path: 'login' , 
         component: Login 
     },
-   {
-        path: 'dashboard' , 
-        component: Dashboard
-    },
+   
     {
         path: 'task-list' ,
         component: TaskList
